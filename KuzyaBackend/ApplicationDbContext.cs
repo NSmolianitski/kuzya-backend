b0a099dcde4be9352dae7008984bf8dbf98a5016
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-07-06 10:59:02 +0300

    Add testing ingredients

--- KuzyaBackend/ApplicationDbContext.cs
+++ KuzyaBackend/ApplicationDbContext.cs
@@ -5,11 +5,43 @@ namespace KuzyaBackend;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Ingredient> Ingredients { get; init; } = null!;
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
         Database.EnsureDeleted();
         Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        AddTestingData(modelBuilder);
+    }
+
+    private void AddTestingData(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ingredient>()
+            .HasData(
+                new Ingredient
+                {
+                    Id = 1,
+                    Name = "Apple",
+                    AvatarId = 1,
+                    Calories = 100,
+                    Proteins = 10,
+                    Fats = 10,
+                    Carbohydrates = 10
+                },
+                new Ingredient
+                {
+                    Id = 2,
+                    Name = "Banana",
+                    AvatarId = 1,
+                    Calories = 200,
+                    Proteins = 5,
+                    Fats = 1,
+                    Carbohydrates = 20
+                }
+            );
     }
 }
