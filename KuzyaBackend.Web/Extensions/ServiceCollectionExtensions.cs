b35775c801026b9cdb4f534cba9110d58e1ab460
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-07-28 19:30:51 +0300

    Add service collection extensions

---
+++ KuzyaBackend.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using KuzyaBackend.DataAccess;
+using KuzyaBackend.DataAccess.Implementations;
+using KuzyaBackend.DataAccess.Interfaces;
+using KuzyaBackend.Services.Implementations;
+using KuzyaBackend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace KuzyaBackend.Web.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatabaseContext(this IServiceCollection services)
+    {
+        var host = Environment.GetEnvironmentVariable("DATABASE_HOST");
+        var port = Environment.GetEnvironmentVariable("DATABASE_PORT");
+        var database = Environment.GetEnvironmentVariable("DATABASE_NAME");
+        var user = Environment.GetEnvironmentVariable("DATABASE_USER");
+        var password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+
+        var connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password};";
+        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+        return services;
+    }
+    
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IIngredientRepository, IngredientRepository>();
+        services.AddScoped<IRecipeRepository, RecipeRepository>();
+        return services;
+    }
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IIngredientService, IngredientService>();
+        services.AddScoped<IRecipeService, RecipeService>();
+        return services;
+    }
+}
--- KuzyaBackend.Web/Program.cs
+++ KuzyaBackend.Web/Program.cs
@@ -1,9 +1,4 @@
-using KuzyaBackend.DataAccess;
-using KuzyaBackend.DataAccess.Implementations;
-using KuzyaBackend.DataAccess.Interfaces;
-using KuzyaBackend.Services.Implementations;
-using KuzyaBackend.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
+using KuzyaBackend.Web.Extensions;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,24 +17,9 @@ builder.Services.AddControllers();
 builder.Configuration.AddEnvironmentVariables();
 builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json");
 
-// Database environment variables
-var host = Environment.GetEnvironmentVariable("DATABASE_HOST");
-var port = Environment.GetEnvironmentVariable("DATABASE_PORT");
-var database = Environment.GetEnvironmentVariable("DATABASE_NAME");
-var user = Environment.GetEnvironmentVariable("DATABASE_USER");
-var password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
-
-// Connection string
-var connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password};";
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
-
-// Repositories
-builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
-builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
-
-// Services
-builder.Services.AddScoped<IIngredientService, IngredientService>();
-builder.Services.AddScoped<IRecipeService, RecipeService>();
+builder.Services.AddDatabaseContext();
+builder.Services.AddRepositories();
+builder.Services.AddServices();
 
 var app = builder.Build();
 
